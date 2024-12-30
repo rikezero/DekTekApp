0ad36ab53e18b8864b52aa3fe8d3f95fd9cd5fd8
Rike <rikezero@gmail.com>
2024-12-30 16:29:16 -0300

    chore: testing if odd behaviour was caused by weird syntax

--- changelog-writter.js
+++ changelog-writter.js
@@ -1,20 +1,5 @@
 const writerOpts = {
   transform: (commit, context) => {
-    const typeToSectionMap = {
-      "BREAKING CHANGE": "⚠️ Major Changes",
-      "refactor": "⚠️ Major Changes",
-      "fix": "🐛 Bug Fixes",
-      "hotfix": "🐛 Bug Fixes",
-      "feat": "✨ New Features",
-      "docs": "🛠️ Miscellaneous",
-      "style": "🛠️ Miscellaneous",
-      "test": "🛠️ Miscellaneous",
-      "chore": "🛠️ Miscellaneous",
-    };
-
-    // Assign section based on type
-    commit.section = typeToSectionMap[commit.type] || "🛠️ Miscellaneous";
-
     // Map PR numbers to links
     if (commit.pullRequest && commit.pullRequest.number) {
       commit.pullRequestLink = `[PR #${commit.pullRequest.number}](${context.repository}/pull/${commit.pullRequest.number})`;
@@ -30,13 +15,13 @@ const writerOpts = {
 
     return commit;
   },
-  groupBy: "section", // Group commits by `section` field
-  commitPartial: "- {{typeFormatted}}: {{section}} {{subject}} {{#if pullRequestLink}}({{pullRequestLink}}){{else}}({{commitLink}}){{/if}}",
+  groupBy: "type", // Group commits by `section` field
+  commitPartial: "- {{typeFormatted}}: {{}} {{subject}} {{#if pullRequestLink}}({{pullRequestLink}}){{else}}({{commitLink}}){{/if}}",
   mainTemplate: `
     {{> header}}
 
-    {{#each groups}}
-    ## {{title}} <!-- Section Title -->
+    {{#each commitGroups}}
+    ## {{section}} <!-- Section Title -->
     {{#each commits}}
     {{> commit}}
     {{/each}}
