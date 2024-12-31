f90e6f5b7b6cd5edd1d659cb0bb649f3f7305912
Rike <rikezero@gmail.com>
2024-12-31 15:35:07 -0300

    chore: changed how we format changelog

--- release-it.config.js
+++ release-it.config.js
@@ -15,25 +15,42 @@ module.exports = {
     publish: false,
   },
   plugins: {
-    "@release-it/conventional-changelog": {
-      strictSemVer: true,
-      infile: "CHANGELOG.md",
-      header: "# DekTek Changelog\n\nAll notable changes are listed below.",
-      preset: {
-        name: "conventionalcommits",
-        types: [
-          { type: "BREAKING CHANGE", section: "Major Changes" },
-          { type: "refactor", section: "Major Changes" },
-          { type: "fix", section: "Bug Fixes" },
-          { type: "hotfix", section: "Bug Fixes" },
-          { type: "feat", section: "New Features" },
-          { type: "docs", section: "Miscellaneous" },
-          { type: "chore", section: "Miscellaneous" },
-          { type: "style", section: "Miscellaneous" },
-          { type: "test", section: "Miscellaneous" },
-        ],
+      "@release-it/conventional-changelog": {
+        strictSemVer: true,
+        infile: "CHANGELOG.md",
+        header: "# DekTek Changelog\n\nAll notable changes are listed below.",
+        preset: {
+            name: "conventionalcommits",
+            types: [
+              { type: "BREAKING CHANGE", section: "⚠️ Major Changes" },
+              { type: "refactor", section: "⚠️ Major Changes" },
+              { type: "fix", section: "🐛 Bug Fixes" },
+              { type: "hotfix", section: "🐛 Bug Fixes" },
+              { type: "feat", section: "✨ New Features" },
+              { type: "docs", section: "🛠️ Miscellaneous" },
+              { type: "style", section: "🛠️ Miscellaneous" },
+              { type: "test", section: "🛠️ Miscellaneous" },
+              { type: "chore", section: "🛠️ Miscellaneous" },
+            ],
+        },
+        writerOpts: {
+            finalizeContext: (context) => {
+            const allowedSections = [
+                "⚠️ Major Changes",
+                "🐛 Bug Fixes",
+                "✨ New Features",
+                "🛠️ Miscellaneous"
+            ];
+            const filteredGroups = (context.commitGroups || []).filter((group) =>
+                allowedSections.includes(group.title)
+            );
+
+            return {
+                ...context,
+                commitGroups: filteredGroups,
+            };
+          },
+        },
       },
-      writerOpts: "./changelog-writer.js",
-    },
   },
 };
