df3b4517bf3f504d5d3104e572da1b134bf1a8ad
Rike <rikezero@gmail.com>
2024-12-31 00:41:44 -0300

    fix: trying syntax changes

--- changelog-writter.js
+++ changelog-writter.js
@@ -1,52 +1,55 @@
-const Handlebars = require("handlebars");
-
-// Register the `or` helper
-Handlebars.registerHelper("or", (a, b1, b2) => a === b1 || a === b2);
-
-const writerOpts = {
+module.exports = {
   transform: (commit, context) => {
-    // Map PR numbers to links
-    if (commit.pullRequest && commit.pullRequest.number) {
+    if (commit.pullRequest?.number) {
       commit.pullRequestLink = `[PR #${commit.pullRequest.number}](${context.repository}/pull/${commit.pullRequest.number})`;
     }
-
-    // Map commit hashes to links
     if (commit.hash) {
       commit.commitLink = `[${commit.hash.substring(0, 7)}](${context.repository}/commit/${commit.hash})`;
     }
-
-    // Format the type for display
-    commit.typeFormatted = commit.type.charAt(0).toUpperCase() + commit.type.slice(1);
+    if (["BREAKING CHANGE", "refactor"].includes(commit.type)) {
+      commit.section = "Major Changes";
+    } else if (["fix", "hotfix"].includes(commit.type)) {
+      commit.section = "Bug Fixes";
+    } else if (commit.type === "feat") {
+      commit.section = "New Features";
+    } else {
+      commit.section = "Miscellaneous";
+    }
+    commit.subject = commit.subject?.trim();
 
     return commit;
   },
-  groupBy: "type", // Group commits by type
-  commitGroupsSort: "title", // Sort groups by title
-  commitsSort: ["type", "scope"], // Sort commits within groups
-  commitPartial: "- {{typeFormatted}}: {{subject}} {{#if pullRequestLink}}({{pullRequestLink}}){{else}}({{commitLink}}){{/if}}",
+  groupBy: "section",
+  commitGroupsSort: (a, b) => {
+    const order = ["Major Changes", "Bug Fixes", "New Features", "Miscellaneous"];
+    return order.indexOf(a.title) - order.indexOf(b.title);
+  },
+  commitsSort: ["scope", "subject"],
   mainTemplate: `
-    {{> header}}
-    {{#each commitGroups}}
-      {{#if (eq this.title "feat")}}
-        New Features
-      {{else if (eq this.title "BREAKING CHANGE" "refactor")}}
-        Major Changes
-      {{else if (or this.title "fix" "hotfix")}}
-        Bug Fixes
-      {{else}}
-        Miscellaneous
-      {{/if}}
-      {{#each commits}}
-        {{> commit}}
-      {{/each}}
-    {{/each}}
-    {{> footer}}
+{{> header}}
+
+{{#each commitGroups}}
+## {{title}}
+{{#each commits}}
+{{> commit}}
+{{/each}}
+
+{{/each}}
+{{> footer}}
   `,
   headerPartial: `
-    # DekTek Changelog
-    All notable changes are listed below.
-  `,
-  footerPartial: ``,
-};
+# DekTek Changelog
 
-module.exports = writerOpts;
+All notable changes are listed below.
+`,
+  commitPartial: `
+- {{subject}}
+  {{#if pullRequestLink}}
+    ({{pullRequestLink}})
+  {{else if commitLink}}
+    ({{commitLink}})
+  {{/if}}
+`,
+  footerPartial: `
+`,
+};
--- release-it.config.js
+++ release-it.config.js
@@ -17,49 +17,21 @@ module.exports = {
   plugins: {
     "@release-it/conventional-changelog": {
       infile: "CHANGELOG.md",
-      header: "# DekTek Changelog\n\nAll notable changes are listed below.",
       preset: {
         name: "conventionalcommits",
         types: [
-          {
-            type: "BREAKING CHANGE",
-            section: "Major Changes",
-          },
-          {
-            type: "refactor",
-            section: "Major Changes",
-          },
-          {
-            type: "fix",
-            section: "Bug Fixes",
-          },
-          {
-            type: "hotfix",
-            section: "Bug Fixes",
-          },
-          {
-            type: "feat",
-            section: "New Features",
-          },
-          {
-            type: "docs",
-            section: "Miscellaneous",
-          },
-          {
-            type: "style",
-            section: "Miscellaneous",
-          },
-          {
-            type: "test",
-            section: "Miscellaneous",
-          },
-          {
-            type: "chore",
-            section: "Miscellaneous",
-          },
+          { type: "BREAKING CHANGE", section: "Major Changes" },
+          { type: "refactor", section: "Major Changes" },
+          { type: "fix", section: "Bug Fixes" },
+          { type: "hotfix", section: "Bug Fixes" },
+          { type: "feat", section: "New Features" },
+          { type: "docs", section: "Miscellaneous" },
+          { type: "chore", section: "Miscellaneous" },
+          { type: "style", section: "Miscellaneous" },
+          { type: "test", section: "Miscellaneous" },
         ],
       },
-      writerOpts: "./changelog-writter.js",
+      writerOpts: "./changelog-writer.js",
     },
   },
 };
